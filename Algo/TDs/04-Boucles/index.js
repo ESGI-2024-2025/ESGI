496197171e23a21b5861b3cc5b7c5b8f8f982777
Cléry Arque-Ferradou <cleeryy@mac.local>
2024-09-25 12:07:56 +0200

    [+] Fixed exo error TD04

--- Algo/TDs/04-Boucles/index.js
+++ Algo/TDs/04-Boucles/index.js
@@ -10,18 +10,30 @@ function main() {
   let newPrice = initialPrice;
   let spectators = waitedSpectator;
 
+  let maxTotal = 0;
+  let maxIndividualPrice = 0;
+  let maxTotalSpectator = 0;
+
   while (spectators < maxSpectator) {
     newPrice = newPrice - 0.6;
     spectators = spectators + 100;
+
+    let total = newPrice * spectators;
+    if (total > maxTotal) {
+      maxTotal = total;
+      maxIndividualPrice = newPrice;
+      maxTotalSpectator = spectators;
+    }
   }
 
   setContent(
     "Le prix du billet est de " +
-      newPrice +
+      maxIndividualPrice +
       "€ pour " +
-      spectators +
+      maxTotalSpectator +
       " spectateurs."
   );
+  setContent("La recette est de " + maxTotal + "€.");
 }
 
 function setTitle() {
