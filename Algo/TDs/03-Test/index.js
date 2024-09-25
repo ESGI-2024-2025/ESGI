805479fb3f238b6fc3c4e0dc895dc90fe89bc1b8
Cléry Arque-Ferradou <cleeryy@mac.local>
2024-09-25 10:33:26 +0200

    [+] JS TD 03

--- Algo/TDs/03-Test/index.js
+++ Algo/TDs/03-Test/index.js
@@ -1,14 +1,23 @@
-"use strict";
-/* CDC
- */
 var title = "TD 03 - Test"; // à changer à chaque TD (par l'objectif pédagogique)
 
 function main() {
-  let a = 23;
-  let b = 45;
+  let year = 2024; // Example year
+  let result = estBissextile(year) ? "est bissextile" : "n'est pas bissextile";
 
   // Vous pouvez utiliser cette fonction pour afficher un résultat sur la page.
-  setContent("a = " + a + " et b = " + b);
+  setContent("L'année " + year + " " + result);
+}
+
+function estBissextile(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function setTitle() {
