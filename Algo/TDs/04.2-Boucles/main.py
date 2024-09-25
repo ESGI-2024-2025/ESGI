e0fb0fe7c94c4d9d00bd1a4251f2c8bdc1055b83
Léo Jason Coulais <leo.jascoulais@gmail.com>
2024-09-25 14:35:01 +0200

    change variable iterator to height

--- Algo/TDs/04.2-Boucles/main.py
+++ Algo/TDs/04.2-Boucles/main.py
@@ -2,16 +2,16 @@
 def main() -> int:
     
     rope = 200
-    i = 0
+    height = 0
     space = 0
 
-    while i<rope:
+    while height<rope:
 
-        if i*(rope-2*i) < space:
-            return i-1
+        if height*(rope-2*height) < space:
+            return height-1
 
-        space = i*(rope-2*i)
-        i += 1
+        space = height*(rope-2*height)
+        height += 1
 
 if __name__ == "__main__":
     print(main())
