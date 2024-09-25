026cbe51fc79f794ff9dc9087a5d64cf8a43ee05
Léo Jason Coulais <leo.jascoulais@gmail.com>
2024-09-25 10:41:51 +0200

    PY TD 03

--- Algo/TDs/03-Test/main.py
+++ Algo/TDs/03-Test/main.py
@@ -1,6 +1,19 @@
 
-def main() -> None:
-    pass
+def main() -> bool:
+    
+    year = input("Entrez votre année à tester: ")
+
+    while not year.isdigit():
+        year = input("Entrez votre année à tester: ")
+    
+    year = int(year)
+
+    if year%4 == 0 and year%100 != 0:
+        return True
+    elif year%400 == 0:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
-    main()
+    print(main())
