f166e8d11aae97ec9e64db4fe775550d80f474cd
= <=>
2024-09-25 09:22:16 +0200

    python file for 00 et 01 tds

---
+++ Algo/TDs/00-Base/main.py
@@ -0,0 +1,14 @@
+
+title = "TD 00"
+
+def main() -> None:
+    a = 23
+    b = 39
+    print("main:",a+b)
+    raise ValueError("main")
+
+def setTitle() -> None:
+    raise NotImplementedError("Is not a web page.")
+
+if __name__ == "__main__":
+    main()
