8778a37bd2125adfb744cded879d132caf4919f9
Léo Jason Coulais <leo.jascoulais@gmail.com>
2024-09-26 11:47:40 +0200

    [+] hash td6

--- Algo/TDs/06 - Hashage/main.py
+++ Algo/TDs/06 - Hashage/main.py
@@ -1,42 +1,97 @@
 
-def fibo_r(nombre_elements=7,suite=[0,1]) -> list:
-
-    if nombre_elements <= 2:
-        return suite
-    else:
-        nouveau_chiffre = suite[-2] + suite[-1]
-        return fibo_r(nombre_elements-1,suite+[nouveau_chiffre])
-
-def fibo_b(nombre_elements=7) -> list:
-
-    nombres_suite = [0,1]
-
-    for _ in range(nombre_elements-2):
-        nouveau_chiffre = nombres_suite[-2] + nombres_suite[-1]
-        nombres_suite.append(nouveau_chiffre)
-    
-    return nombres_suite
+from json import load, dumps
+from os import path
 
 def main() -> list:
     
-    method = input("Voulez vous utiliser la recursive ou les boucles ? [R/B] ")
+    hastable = {}
 
-    while not (method == "R" or method == "B"):
-        method = input("Voulez vous utiliser la recursive ou les boucles ? [R/B] ")
+    while True:
 
-    nombre = input("Combien de valeurs voulez vous ? ")
+        action = input("\nVoulez vous récuperer un departement, en créer un, sauvegarder la table ou l'importer ? [R, C, S, I] ").capitalize()
 
-    while not nombre.isdigit():
-        nombre = input("Combien de valeurs voulez vous ? ")
-    
-    nombre = int(nombre)
+        if action == "R":
 
-    if method == "R":
-        nombres_suite = fibo_r(nombre)
-    else:
-        nombres_suite = fibo_b(nombre)
+            departement = input("\nQuel departement recuperer ? ")
 
-    return nombres_suite
+            while not departement.isdigit():
+                departement = input("Quel departement recuperer ? ")
+
+            if departement in hastable:
+                print("Chef-lieu :",hastable[departement][0])
+                print("Nb habitants (millions) :",hastable[departement][1])
+                print("Departement :",hastable[departement][2])
+                print("Lieu emblematique :",hastable[departement][3])
+
+            else:
+                print("Ce departement n'est pas disponible dans la table.")
+                continue
+
+        elif action == "C":
+
+            num = input("\nQuel departement créer ? ")
+
+            while not num.isdigit():
+                num = input("Quel departement créer ? ")
+
+            if not num in hastable:
+
+                chef = input("\nEntrez un chef lieu: ")
+
+                while chef.isdigit():
+                    chef = input("Entrez un chef lieu: ")
+
+                wrong = True
+
+                while wrong:
+                    nb = input("Entrez un nombre d'habitants (millions): ")
+                
+                    try:
+                        nb = float(nb)
+                        wrong = False
+                    except:
+                        pass
+                
+                departement = input("Entrez un departement: ")
+
+                while departement.isdigit():
+                    departement = input("Entrez un departement: ")
+
+                place = input("Entrez une place emblematique: ")
+
+                while place.isdigit():
+                    place = input("Entrez une place emblatique: ")
+
+                hastable[num] = [chef, nb, departement, place]
+
+            else:
+                print("Ce departement existe déjà dans la table.")
+                continue
+
+        elif action == "S":
+
+            name = input("Quel nom donner au fichier JSON ? ")
+
+            with open(name+".json","w") as file:
+                file.write(dumps(hastable,indent=4))
+
+            print("BDD sauvegardée !")
+
+        elif action == "I":
+
+            name = input("Quel est le nom du fichier JSON ? ")
+
+            while not path.exists(name+".json"):
+                name = input("Ce fichier n'existe pas.\nQuel est le nom du fichier JSON ? ")
+
+            with open(name+".json") as file:
+                hastable = load(file)
+
+            print("BDD importée !")
+
+        else:
+            print("Le choix n'est pas valide.")
+            continue
 
 if __name__ == "__main__":
     print(main())
